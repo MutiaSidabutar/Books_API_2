f095afbeebb37205ca8f99cc76ab9d7f34476684
MutiaSidabutar <mutiasidabutar2006@gmail.com>
2025-12-01 15:27:42 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const {pool} = require("pg");
+const { Pool } = require("pg");
 //?.env+ menyimpan data konfigurasu agar tidak langsung ditulidkan dalam kode
 //? Dapat disebut juga variabel "global"
 const dotenv = require("dotenv");
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 //?koneksi ke postgressSQL
-const pool = new pool({
+const pool = new Pool({
     User : process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
